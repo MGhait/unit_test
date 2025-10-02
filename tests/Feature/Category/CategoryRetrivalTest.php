36785ac2ed60c9d5daa6008d71ae3eee68e9f19b
Mohamed Saad <mosaad05019@gmail.com>
2025-10-02 20:22:27 +0300

    user-refator

--- tests/Feature/Category/CategoryRetrivalTest.php
+++ tests/Feature/Category/CategoryRetrivalTest.php
@@ -11,42 +11,54 @@ use App\Models\User;
 class CategoryRetrivalTest extends TestCase
 {
     use RefreshDatabase;
+    protected User $user;
+    protected function setUp(): void{
+        parent::setUp();
+        $this->user = User::factory()->create();
+    }
     /**
      * A basic feature test example.
      */
     public function test_check_if_categories_page_opens_successfully(): void
     {
-        $user = User::factory()->create();
-        $response = $this->actingAs($user)->get('/categories');
+        // $user = $this->getUser();
 
-        $response->assertStatus(200);
-        $response->assertViewIs('categories.index');
-        $response->assertSeeText('Add New Category');
+        $response = $this->actingAs($this->user)->get('/categories');
+
+        $response->assertStatus(200)
+            ->assertViewIs('categories.index')
+            ->assertSeeText('Add New Category');
     }
 
     public function test_check_if_categories_page_contains_categories()
     {
         Category::factory()->count(4)->create();
+        // $user = $this->getUser();
 
-        $user = User::factory()->create();
-        $response = $this->actingAs($user)->get('/categories');
-        $response->assertViewHas('categories', function($categories) {
+        $response = $this->actingAs($this->user)->get('/categories');
+
+        $response->assertViewHas('categories', function ($categories) {
             return $categories->count() === 4;
         });
     }
     public function test_check_if_pagination_works_as_expected()
     {
         Category::factory()->count(15)->create();
+        // $user = $this->getUser();
 
-        $user = User::factory()->create();
+        $response = $this->actingAs($this->user)->get('/categories');
 
-        $response = $this->actingAs($user)->get('/categories');
-        $response->assertViewHas('categories', function($categories) {
+        $response->assertViewHas('categories', function ($categories) {
             return $categories->count() === 10;
         });
-        $response = $this->actingAs($user)->get('/categories?page=2');
-        $response->assertViewHas('categories', function($categories) {
+        $response = $this->actingAs($this->user)->get('/categories?page=2');
+        $response->assertViewHas('categories', function ($categories) {
             return $categories->count() === 5;
         });
     }
+
+//    public function getUser()
+//    {
+//        return User::factory()->create();
+//    }
 }
