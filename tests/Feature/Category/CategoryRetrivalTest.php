6ec6662504cb32ed3efa99914cd73280a70d6ea7
Mohamed Saad <mosaad05019@gmail.com>
2025-10-02 20:24:35 +0300

    acting-as-refactor

--- tests/Feature/Category/CategoryRetrivalTest.php
+++ tests/Feature/Category/CategoryRetrivalTest.php
@@ -11,19 +11,18 @@ use App\Models\User;
 class CategoryRetrivalTest extends TestCase
 {
     use RefreshDatabase;
-    protected User $user;
+//    protected User $user;
     protected function setUp(): void{
         parent::setUp();
-        $this->user = User::factory()->create();
+        $this->actingAs(User::factory()->create());
+//        $this->user = User::factory()->create();
     }
-    /**
-     * A basic feature test example.
-     */
+
     public function test_check_if_categories_page_opens_successfully(): void
     {
         // $user = $this->getUser();
 
-        $response = $this->actingAs($this->user)->get('/categories');
+        $response = $this->get('/categories');
 
         $response->assertStatus(200)
             ->assertViewIs('categories.index')
@@ -35,7 +34,7 @@ class CategoryRetrivalTest extends TestCase
         Category::factory()->count(4)->create();
         // $user = $this->getUser();
 
-        $response = $this->actingAs($this->user)->get('/categories');
+        $response = $this->get('/categories');
 
         $response->assertViewHas('categories', function ($categories) {
             return $categories->count() === 4;
@@ -46,12 +45,12 @@ class CategoryRetrivalTest extends TestCase
         Category::factory()->count(15)->create();
         // $user = $this->getUser();
 
-        $response = $this->actingAs($this->user)->get('/categories');
+        $response = $this->get('/categories');
 
         $response->assertViewHas('categories', function ($categories) {
             return $categories->count() === 10;
         });
-        $response = $this->actingAs($this->user)->get('/categories?page=2');
+        $response = $this->get('/categories?page=2');
         $response->assertViewHas('categories', function ($categories) {
             return $categories->count() === 5;
         });
